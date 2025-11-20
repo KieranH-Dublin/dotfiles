216029e01e61fa2a62a752e1025927530c30009d
Kieran Hyer <kieran.hyer.business@proton.me>
2025-11-20 13:58:32 +0000

    change a comment

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -3,8 +3,7 @@
 -- Use <Esc> to exit terminal mode
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
-
--- Map <C-j>, <C-k>, <C-h>, <C-l> to navigate between windows in any modes
+-- Navigate windows with <Control-direction>
 vim.keymap.set({ 'n' }, '<C-h>', '<C-w>h')
 vim.keymap.set({ 'n' }, '<C-j>', '<C-w>j')
 vim.keymap.set({ 'n' }, '<C-k>', '<C-w>k')
