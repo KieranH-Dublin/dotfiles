d9bac6ad791d5398f6fc7a89a7a339abfaa359dc
Kieran Hyer <kieran.hyer.business@proton.me>
2025-11-21 10:37:38 +0000

    use treesitter main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = 'main',
+    build = ":TSUpdate",
 }
 
