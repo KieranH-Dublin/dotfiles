8a94d245844662fe99a8b8d5b550c58433c5e6da
Kieran Hyer <kieran.hyer.business@proton.me>
2025-11-21 09:59:33 +0000

    mason-lspconfig

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,22 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { 
+                "pyright",
+                "ruff",
+                "cssls",
+                "html",
+                "dockerls",
+                "docker_compose_language_service",
+                "postgres_lsp",
+                "marksman",
+                "jsonls",
+                "jqls",
+        },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
 }
 
